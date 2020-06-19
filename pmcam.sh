b48f1e4004c1ede77f65bcd21576e7b317f32d80
jaclu <jacob.lundqvist@gmail.com>
2020-06-19 18:03:52 +0200

    two new params TIME_STAMP & RESIZE and some code improvements

--- pmcam.sh
+++ pmcam.sh
@@ -3,12 +3,29 @@
 #
 # Primary variables you might want to change
 #
-OUTPUT_DIR=../changed_imgs
-DIFF_LIMIT=19
+DIFF_LIMIT=1400
+OUTPUT_DIR=../changed_$DIFF_LIMIT
 SOURCE_WILDCARD=*.JPG
 
+#
+# For TIME_STAMP & RESIZE, set them to "" if the feature is
+# not desired.
+#
 
-DIFF_IMG=diff.png
+# you will probably need to play arround with -pointsize and offset in
+# -annotate, depending on your image size...
+TIME_STAMP="-fill white -gravity SouthWest -pointsize 100 -annotate +40+25 %[exif:DateTimeOriginal]"
+#TIME_STAMP=""
+
+#RESIZE=""
+RESIZE="-resize 1280x720"
+
+
+#
+# Settings normally not needed to be changed below
+#
+
+DIFF_IMG=$OUTPUT_DIR/diff.png
 DIFF_RESULT_FILE=$OUTPUT_DIR/diff_results.txt
 
 fn_cleanup() {
@@ -22,23 +39,38 @@ fn_terminate_script() {
 }
 trap 'fn_terminate_script' SIGINT
 
+fn_copy_image() {
+    if [ "$TIME_STAMP $RESIZE" != " " ] ; then
+	convert $1 $TIME_STAMP $RESIZE $2/$1
+    else
+	cp $1 $2
+    fi
+}
+
+
 mkdir -p $OUTPUT_DIR
 PREVIOUS_IMAGE=""
 for NEXT_IMAGE in $SOURCE_WILDCARD ; do    
     if [[ "$PREVIOUS_IMAGE" != "" ]]; then
+	fn_copy_image "$NEXT_IMAGE" $OUTPUT_DIR
+	CURENT_IMAGE="$OUTPUT_DIR/$NEXT_IMAGE"
 	# For some reason, `compare` outputs the result to stderr so
 	# it's not possibly to directly get the result. It needs to be
 	# redirected to a temp file first.
-	compare -fuzz 20% -metric ae "$PREVIOUS_IMAGE" "$NEXT_IMAGE" $DIFF_IMG 2> $DIFF_RESULT_FILE
+	compare -fuzz 20% -metric ae "$PREVIOUS_IMAGE" "$CURENT_IMAGE" $DIFF_IMG 2> $DIFF_RESULT_FILE
 	DIFF="$(cat $DIFF_RESULT_FILE)"
-	fn_cleanup
-	if [ "$DIFF" -gt $DIFF_LIMIT ]; then
-	    cp "$NEXT_IMAGE" $OUTPUT_DIR
-	    echo "keep $NEXT_IMAGE (Diff = $DIFF)"
-	    PREVIOUS_IMAGE="$NEXT_IMAGE"
+
+	if [ "$DIFF" -lt $DIFF_LIMIT ]; then
+	    # use $MEXT_IMAGE over $CURENT_IMAGE in echo to skip path
+	    echo "   skipped $NEXT_IMAGE - $DIFF"
+	    rm "$CURENT_IMAGE"
+	else
+	    # use $MEXT_IMAGE over $CURENT_IMAGE in echo to skip path
+	    echo "keep $NEXT_IMAGE - $DIFF"
+	    PREVIOUS_IMAGE="$CURENT_IMAGE"
 	fi
     else
-	cp "$NEXT_IMAGE" $OUTPUT_DIR
-	PREVIOUS_IMAGE="$NEXT_IMAGE"
+	fn_copy_image "$NEXT_IMAGE" $OUTPUT_DIR
+	PREVIOUS_IMAGE="$OUTPUT_DIR/$NEXT_IMAGE"
     fi
 done
